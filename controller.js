4eafb59be74076f0d8f24cb9b98c750528c1370b
Long Hoang <long@mindworker.de>
2022-02-27 15:36:14 +0800

    Rename camp to isolate

--- controller.js
+++ controller.js
@@ -1,11 +1,11 @@
 let arrive_frame = document.getElementById('frame-arrive');
 let test_frame = document.getElementById('frame-test');
-let camp_frame = document.getElementById('frame-camp');
+let isolate_frame = document.getElementById('frame-isolate');
 let release_frame = document.getElementById('frame-release');
 
 let arrive_queue = new Queue();
 let test_queue = new Queue();
-let camp_queue = new Queue();
+let isolate_queue = new Queue();
 let release_queue = new Queue(); // not needed but keeps code consistent
 
 function new_arrival(num_arrivals) {
@@ -23,7 +23,7 @@ function render_person_queue(queue, frame) {
 function render() {
   render_person_queue(arrive_queue, arrive_frame);
   render_person_queue(test_queue, test_frame);
-  render_person_queue(camp_queue, camp_frame);
+  render_person_queue(isolate_queue, isolate_frame);
 }
 
 function set_transition_btn_event(btn_id, src_queue, dst_queue) {
@@ -39,11 +39,11 @@ function set_transition_btn_event(btn_id, src_queue, dst_queue) {
 }
 
 set_transition_btn_event('arrive-test-btn', arrive_queue, test_queue);
-set_transition_btn_event('arrive-camp-btn', arrive_queue, camp_queue);
+set_transition_btn_event('arrive-isolate-btn', arrive_queue, isolate_queue);
 set_transition_btn_event('arrive-release-btn', arrive_queue, release_queue);
-set_transition_btn_event('test-camp-btn', test_queue, camp_queue);
+set_transition_btn_event('test-isolate-btn', test_queue, isolate_queue);
 set_transition_btn_event('test-release-btn', test_queue, release_queue);
-set_transition_btn_event('camp-release-btn', camp_queue, release_queue);
+set_transition_btn_event('isolate-release-btn', isolate_queue, release_queue);
 
 function init() {
   new_arrival(5);
