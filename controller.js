80599e49eb5ab8cdf0e9567f76a21ac26a980003
Long Hoang <long@mindworker.de>
2022-02-27 15:17:20 +0800

    Refactor queue transitions and event listeners

--- controller.js
+++ controller.js
@@ -1,76 +1,49 @@
-let arrival_frame = document.getElementById('frame-arrive');
+let arrive_frame = document.getElementById('frame-arrive');
 let test_frame = document.getElementById('frame-test');
 let camp_frame = document.getElementById('frame-camp');
 let release_frame = document.getElementById('frame-release');
 
-let arrival_queue = new Queue();
+let arrive_queue = new Queue();
 let test_queue = new Queue();
 let camp_queue = new Queue();
+let release_queue = new Queue(); // not needed but keeps code consistent
 
 function new_arrival(num_arrivals) {
   for (let i = 0; i < num_arrivals; i++) {
-    arrival_queue.enqueue(new Person());
+    arrive_queue.enqueue(new Person());
   }
   render();
 }
 
+function render_person_queue(queue, frame) {
+  frame.replaceChildren(); // TODO get rid of this
+  queue.data.forEach(p => p.attach(frame))
+}
+
 function render() {
-  arrival_frame.replaceChildren();
-  test_frame.replaceChildren();
-  camp_frame.replaceChildren();
-  release_frame.replaceChildren();
-  
-  arrival_queue.data.forEach(p => {
-    p.attach(arrival_frame);
-  });
-  
-  test_queue.data.forEach(p => {
-    p.attach(test_frame);
-  });
-  
-  camp_queue.data.forEach(p => {
-    p.attach(camp_frame);
-  });
+  render_person_queue(arrive_queue, arrive_frame);
+  render_person_queue(test_queue, test_frame);
+  render_person_queue(camp_queue, camp_frame);
 }
 
-new_arrival(5);
-
-let button = document.getElementById('btn1');
-Rx.Observable.fromEvent(button, 'click').subscribe(value => {
-  persons[0].attach(test_frame);
-});
-
-let arrive_test_btn = document.getElementById('arrive-test-btn');
-Rx.Observable.fromEvent(arrive_test_btn, 'click').subscribe(value => {
-  console.log('Arrival -> Test')
-  // TODO handle case when empty
+function set_transition_btn_event(btn_id, src_queue, dst_queue) {
+  let btn = document.getElementById(btn_id);
+  Rx.Observable.fromEvent(btn, 'click').subscribe(value => {
+    // TODO log
+    // TODO handle case when empty
   
-  let p = arrival_queue.dequeue();
-  test_queue.enqueue(p);
-  render();
-});
-
-let arrive_camp_btn = document.getElementById('arrive-camp-btn');
-Rx.Observable.fromEvent(arrive_camp_btn, 'click').subscribe(value => {
-  console.log('Arrival -> Camp')
-  // TODO handle case when empty
-  
-  let p = arrival_queue.dequeue();
-  camp_queue.enqueue(p);
-  render();
-});
-
-function release_person(person) {
-  console.log('Person ' + person._id + ' released');
+    let p = src_queue.dequeue();
+    dst_queue.enqueue(p);
+    render();
+  })
 }
 
-let arrive_release_btn = document.getElementById('arrive-release-btn');
-Rx.Observable.fromEvent(arrive_release_btn, 'click').subscribe(value => {
-  console.log('Arrival -> Release')
-  // TODO handle case when empty
-  
-  let p = arrival_queue.dequeue();
-  release_person(p);
-  render();
-});
+set_transition_btn_event('arrive-test-btn', arrive_queue, test_queue);
+set_transition_btn_event('arrive-camp-btn', arrive_queue, camp_queue);
+set_transition_btn_event('arrive-release-btn', arrive_queue, release_queue);
 
+function init() {
+  new_arrival(5);
+}
+
+init();
